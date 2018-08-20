53e8f472dee1826ffc86883638050eb85d915a28
coolguyethanw <42422230+coolguyethanw@users.noreply.github.com>
2018-08-20 12:10:19 -1000

    h

---
+++ rnggenerator.js
@@ -0,0 +1,5 @@
+var Adj = ["is cool","likes mr.kam","loves jake paul","has ligma rip","has sugondese","loves anime","is sick af","is homeless","is wack","is T H I C C","is gay(happy)","eats food","IS A CAPITOLIST AND WILL BE SENT TO GULAG","h","is a spy",2,1,true];
+
+var AskName = prompt("Enter your name");
+var choose = (Math.floor(Math.random()*Adj.length));
+window.alert(AskName + " " + Adj[choose] + "!" );
